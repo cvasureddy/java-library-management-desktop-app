920c1acc450f35cea1ae74d9b51220a4c81ca7e1
cvasureddy <139790728+cvasureddy@users.noreply.github.com>
2026-03-01 13:09:25 +0530

    Add files via upload

---
+++ Constants.java
@@ -0,0 +1,4 @@
+package com.library.util;
+public class Constants {
+    public static final String APP_NAME="Library Management System";
+}
---
+++ DatabaseConnection.java
@@ -0,0 +1,4 @@
+package com.library.util;
+public class DatabaseConnection {
+    public static void connect(){ System.out.println("DB Connected"); }
+}
